204c8639a28cd32ae8c13b39439b0cd955a71a59
Salma J <87875257+sa1l1@users.noreply.github.com>
2024-02-04 22:57:44 -0500

    Update indexPage.ts

--- src/data/indexPage.ts
+++ src/data/indexPage.ts
@@ -1,6 +1,6 @@
 import type { TIndexPageData } from "@/types/indexPageData";
 const description =
-  "My name is **Salma**, I'm a sophomore in highschool who enjoys computer science, math and lingustics. I've recently gotten into competitive programming as well.";
+  "I'm **Salma**, a sophomore in highschool who enjoys computer science, math and lingustics. I've recently gotten into competitive programming as well.";
 
 const footer = "Welcome !1!";
 export const INDEX_PAGE_DATA: TIndexPageData = {
