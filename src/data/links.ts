efe06004f7c74e17bade1d2438c536ee137f8de7
Salma J <87875257+sa1l1@users.noreply.github.com>
2024-02-05 06:53:15 -0500

    Update links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,7 +2,6 @@ import type { TLinkItem } from "@/types/links";
 
 export const LINKS_DATA: TLinkItem[] = [
   { name: "GitHub", path: "https://github.com/sa1l1" },
-  { name: "Instagram", path: "https://instagram.com/ssszlma" },
   { name: "Codeforces", path: "https://codeforces.com/profile/salsal_ly" },
 
 ];
