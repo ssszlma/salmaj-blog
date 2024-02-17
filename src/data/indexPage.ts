d15b520b5714e6f3be1c58fca13ae6c07d96f498
Salma J <87875257+sa1l1@users.noreply.github.com>
2024-02-17 15:50:07 -0500

    Update indexPage.ts

--- src/data/indexPage.ts
+++ src/data/indexPage.ts
@@ -1,8 +1,8 @@
 import type { TIndexPageData } from "@/types/indexPageData";
 const description =
-  "I'm **Salma**, a sophomore in highschool who enjoys computer science, math, lingustics and their intersections. I've most recently gotten into competitive programming and cybersecurity and hope to learn more in the future.";
+  "I'm **Salma**, a sophomore in highschool who enjoys computer science, math, lingustics and their intersections.";
 
-const footer = "Salma J.";
+const footer = "~ This website is still in the works ~";
 export const INDEX_PAGE_DATA: TIndexPageData = {
   title: "Salma J.",
   description,
