647f793badbe4af17ede59cbdb63dd3badc6a74a
Salma J <87875257+salmly@users.noreply.github.com>
2024-02-19 21:56:59 -0500

    Update indexPage.ts

--- src/data/indexPage.ts
+++ src/data/indexPage.ts
@@ -2,9 +2,9 @@ import type { TIndexPageData } from "@/types/indexPageData";
 const description =
   "I'm **Salma**, a sophomore in highschool who enjoys computer science, math, lingustics and their intersections.";
 
-const footer = "~ This website is still in the works ~";
+const footer = "Welcome !";
 export const INDEX_PAGE_DATA: TIndexPageData = {
-  title: "Salma J.",
+  title: "Salma's blog",
   description,
   footer,
 };
